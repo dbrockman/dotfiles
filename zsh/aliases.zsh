7224fe99622b30b6999c1c2bdfcd1ecaa6799a08
David Brockman Smoliansky <david.bs@gmail.com>
2012-04-24 21:12:25 +0200

    aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,21 @@
 alias reload!='. ~/.zshrc'
+
+alias cd..='cd ..'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias l='ls -Gl'
+alias la='ls -Gla'          # show hidden files
+alias lu='ls -Gltur'        # sort by and show access time, most recent last
+alias lt='ls -Gltr'         # sort by date, most recent last
+alias c='clear'
+alias §='exit'
+
+### Git
+alias gs='git status'
+
+### Node / NPM
+# alias npm-old='npm outdated -g'
+alias npm-up='sudo npm up -g'
+alias npm-i='sudo npm i -g'
+
