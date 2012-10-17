ce902d6ef163c7d074199fcc259bcb78a4b0ef39
David Brockman Smoliansky <david.bs@gmail.com>
2012-10-17 11:40:08 +0200

    Remove Node version from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -72,7 +72,8 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%0/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(node_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+# export PROMPT=$'\n$(node_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
