104cd3e54ee5fff0d8b6ee0ee1b20be6b40e2c2e
David Brockman Smoliansky <david.bs@gmail.com>
2013-03-26 15:45:42 +0100

    Fix duplicate "Open With" in Finder

---
+++ osx/fixow.zsh
@@ -0,0 +1,3 @@
+# Fix duplicate "Open With" in Finder
+
+alias fixow='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user;killall Finder;echo "Open With has been rebuilt, Finder will relaunch"'
