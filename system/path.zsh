5fbbda588e447190ffb2bf7b274640bfd3e7f228
David Brockman Smoliansky <david.bs@gmail.com>
2013-09-30 16:43:37 +0200

    path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
-export PATH="/Applications/Xcode.app/Contents/Developer/usr/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="/Applications/Xcode.app/Contents/Developer/usr/bin:/usr/local/Cellar/ruby/1.9.3-p125/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# Postgres.app
+# export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
