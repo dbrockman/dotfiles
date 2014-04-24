e5568747fc1fffb5e8538f1b391f8c5ac74f7627
David Brockman Smoliansky <david.bs@gmail.com>
2014-04-24 12:41:56 +0200

    Include Python and Xcode bins in path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/python:$ZSH/bin:/Applications/Xcode.app/Contents/Developer/usr/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
